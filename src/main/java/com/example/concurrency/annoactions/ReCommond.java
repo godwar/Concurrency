2ecbf8592fcdd60d84feb8536fafbcd950459ed0
“ <yxking1@163.com>
2018-08-02 10:10:47 +0800

    注解

---
+++ src/main/java/com/example/concurrency/annoactions/ReCommond.java
@@ -0,0 +1,17 @@
+package com.example.concurrency.annoactions;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author Administrator
+ * @Date 2018/8/2
+ * @Description
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ReCommond {
+    String value() default "";
+}
